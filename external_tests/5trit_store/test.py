2241ceb2230ecc89b3b486f5210309cabbeabbd0
Peter <peter@codebuffet.co>
2017-12-31 21:01:08 +0100

    created inversion function!

--- external_tests/5trit_store/test.py
+++ external_tests/5trit_store/test.py
@@ -1,4 +1,18 @@
+def trit_to_int(t_arr):
+    ret = 0
+
+    for i, t in enumerate(t_arr):
+        if t == -1:
+            ret += -1
+        if t == 1:
+            ret += 1
+        if t == 0:
+            ret += 0
+        ret *= 3
+    return ret // 3
+
 def int_to_trit(i, t_arr):
+    #print("i: ", i)
     if i < 0:  # Special case for handling negatives: just flip all the trits in the positive version
         # return [-x for x in int_to_trit(-i, t_arr)]  # the Python way of doing the following:
         # Store the result of running this against a positive number
@@ -12,7 +26,7 @@ def int_to_trit(i, t_arr):
         return t_arr if len(t_arr) > 0 else [0]
     # Python % always returns a positive number; C does not so some logic below will need changed
     r = abs(i % 3)
-    # print "{} % 3 == {}".format(i, r)  # for debug
+    print "{} % 3 == {}".format(i, r)  # for debug
     if r == 2:
         # insert(0, x) simply means to append to the beginning of a list in Python. If using an actual array, this could be accomplished by allocating a new array of size n + 1, setting the first value to -1, and copying the array of size n after the -1 value. Alternatively, the C implementation may allocate an array with 5 indices for this method and pass the current index as a parameter.
         t_arr.insert(0, -1)
@@ -26,8 +40,8 @@ def int_to_trit(i, t_arr):
     # The // operator in Python emphasizes integer division (floors the result)
     return int_to_trit(i // 3, t_arr)
 
-
 if __name__ == "__main__":
-    arr = []
-    print(int_to_trit(-3, arr))
-    print(arr)
+    for i in range(-121, 122):
+        i2 = int_to_trit(i, [])
+        i3 = trit_to_int(i2)
+        print("i: %d list: %s back to int: %d len: %d" % (i, i2, i3, len(i2)))
